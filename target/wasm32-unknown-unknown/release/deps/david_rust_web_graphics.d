9feef14a804f98c97001a4833a8fa8d43d84b967
dp-mason <davidpattenmason@gmail.com>
2022-12-29 13:21:25 -0600

    removed ./ in file path to js file

--- target/wasm32-unknown-unknown/release/deps/david_rust_web_graphics.d
+++ target/wasm32-unknown-unknown/release/deps/david_rust_web_graphics.d
@@ -1,6 +1,6 @@
-/Users/cloudemployee/Desktop/rust/david_rust_web_graphics/target/wasm32-unknown-unknown/release/deps/david_rust_web_graphics.wasm: src/lib.rs src/scene.rs src/scene/camera.rs src/scene/mesh.rs src/transformations.rs src/renderer.rs
+/Users/cloudemployee/Desktop/rust/rust-wasm-3D/target/wasm32-unknown-unknown/release/deps/david_rust_web_graphics.wasm: src/lib.rs src/scene.rs src/scene/camera.rs src/scene/mesh.rs src/transformations.rs src/renderer.rs
 
-/Users/cloudemployee/Desktop/rust/david_rust_web_graphics/target/wasm32-unknown-unknown/release/deps/david_rust_web_graphics.d: src/lib.rs src/scene.rs src/scene/camera.rs src/scene/mesh.rs src/transformations.rs src/renderer.rs
+/Users/cloudemployee/Desktop/rust/rust-wasm-3D/target/wasm32-unknown-unknown/release/deps/david_rust_web_graphics.d: src/lib.rs src/scene.rs src/scene/camera.rs src/scene/mesh.rs src/transformations.rs src/renderer.rs
 
 src/lib.rs:
 src/scene.rs:
